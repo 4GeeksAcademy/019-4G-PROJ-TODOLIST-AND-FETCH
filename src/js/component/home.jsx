476fca604516b5dfb9942cd7c3ce8f36c30de3c2
JerryJox <profesionalmail@msn.com>
2023-05-09 16:15:25 +0200

    upload files

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,104 +1,53 @@
 import React, {useState} from "react";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Home = () => {
-  const [ inputValue, serInputValue] = useState("")
-  const [ todos, setTodos] = useState([])
+  const [inputValue, setInputValue ] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [hoverIndex, setHoverIndex] = useState(-1);
 
-  const 
-
-  return {
-      <div className="container">
-        <h1>My Todos</h1>
-          <ul>
-            <li>
-                <input
-                  type="text"
-                  onChange={(e) => setInputValue(e.target.velue)} 
-                  value={inputValue}
-                  KeyPress={(e) => {
-                      if (e.Key === "Enter") {
-                          setTodos(todos.concat(inputValue));
-                          setInpuValue("");
-                      }
-                  }}
-                  placeholder="What to do?"></input>
-            </li>
-            {todos.map((item, index) => (
-              <li>
-                {item} 
-                <i 
-                  class="fas fa-solid fa-xmark" 
-                  onClick={() => 
-                    setTodos(
-                      todos.filter(
-                        (t, currentIndex) => 
-                          index != currentIndex
-                      )
-                    )
-                }></i>
-              </li>
-            ))}
-            </ul>
-          <div>{todos.length} tasks</div>
-      </div>
-  }
-}
+  return (
+    <div className="container">
+      <h1>My Todos</h1>
+      <ul>
+        <li>
+          <input 
+            type="text" 
+            onChange={(event) => setInputValue(event.target.value)}
+            value={inputValue}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {setTodos(todos.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+            placeholder="what"></input>
+        </li>
+        {todos.map((item, index) => ( 
+          <div 
+            key={index}
+            onMouseEnter={() => setHoverIndex(index)}
+            onMouseLeave={() => setHoverIndex(-1)}
+          >
+            {item} 
+            {hoverIndex === index && (
+              <FontAwesomeIcon 
+                icon={faXmark} 
+                onClick={() => setTodos(todos.filter((todos, currentIndex) => index != currentIndex))}
+              />
+            )}
+          </div>
+        ))}
+      </ul>
+      <div>{todos.length} tasks</div>
+    </div>
+  );
+};
 
 export default Home;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from "react";
-
-// const Home = () => {
-//   const [tasks, setTasks] = useState("");
-//   const [newTask, setNewTask] = useState([]);
-
-//   function handleTask(event) {
-//     if (event.key === "Enter") {
-//       setNewTask([...newTask, tasks]);
-//       setTasks("");
-//     }
-//   }
-
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         value={tasks}
-//         onChange={(event) => setTasks(event.target.value)}
-//         onKeyDown={handleTask}
-//       />
-//       {newTask.length === 0 ? (
-//         <p>No hay tareas, añadir tareas</p>
-//       ) : (
-//         newTask.map((task, index) => <p key={index}>{task}</p>)
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Home;
-
+// Add into Array --> concat
+// Delete from Array  --> filter
+// Update --> map
